cc277aff45f9f5e564b54d1ff63847a0223d8891
Eliana <elianacordoba053@gmail.com>
2024-10-13 01:05:10 -0300

    Cambio 1

---
+++ cosas.ts
@@ -0,0 +1,25 @@
+interface Cosas {
+  id: number;
+  name: string;
+  description: string;
+}
+
+const cosas = {
+  collection: [
+    { id: 1, name: "Objeto 1", description: "Descripción del objeto 1" },
+    { id: 2, name: "Objeto 2", description: "Descripción del objeto 2" },
+    { id: 3, name: "Objeto 3", description: "Descripción del objeto 3" },
+    { id: 4, name: "Objeto 4", description: "Descripción del objeto 4" },
+  ],
+  getAll: function (): Cosas[] {
+    // Tu código
+    // return cosas.collection;
+  },
+  // Método para obtener un objeto por id
+  getById: function (id: number): Cosas | undefined {
+    // Tu código
+    // return cosas.collection.find((c) => c.id === id);
+  },
+};
+
+export default cosas;
