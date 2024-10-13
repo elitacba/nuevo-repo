b3cdc51afb1b4a6ef0116c29257bf9cf0afbc1ae
elitacba <63559633+elitacba@users.noreply.github.com>
2024-10-13 01:19:31 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import cosas from "./cosas.ts";
 function main() {
-  //   const mostrarCosas = cosas.getAll();
-  //   console.log(mostrarCosas);
-  //   const buscarCosas = cosas.getById(1);
-  //   console.log(buscarCosas);
+    const mostrarCosas = cosas.getAll();
+    console.log(mostrarCosas);
+    const buscarCosas = cosas.getById(1);
+    console.log(buscarCosas);
 }
 
 main();
